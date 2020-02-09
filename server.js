4f715e39f058ff6d81a832c874f87fdb41ac550f
Tyler Cloetingh <tcloetingh@gmail.com>
2020-02-09 18:07:55 -0500

    initial routes set up

--- server.js
+++ server.js
@@ -8,6 +8,32 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Showing this bootcamp ${req.params.id}` });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Deleted Bootcamp ${req.params.id}` });
+});
+
 app.listen();
 
 app.listen(
